705d364cd0cadd8240bd207c97aba18e905540cf
Cristobal Grana <cristobal.wee@gmail.com>
2018-06-09 18:43:36 -0500

    Added overlay

---
+++ index.js
@@ -0,0 +1,7 @@
+function openAbout() {
+  document.getElementById('overlay-about').style.display = 'block';
+}
+
+function closeAbout() {
+  document.getElementById('overlay-about').style.display = 'none';
+}
