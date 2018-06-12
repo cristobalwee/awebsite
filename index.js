2f7c517d47151e77bce991b70bd963e0c3cc34ec
Cristobal Grana <cristobal.wee@gmail.com>
2018-06-12 16:34:39 -0400

    Added facts

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function closeAbout() {
   document.getElementById('overlay-about').style.display = 'none';
 }
 
-function openFact() {
+function openFact(fact) {
+  document.getElementById('fact-content').innerHTML = '<h2>' + fact + '</h2>' + '<p>facts</p>';
   document.getElementById('overlay-fact').style.display = 'block';
+}
+
+function closeFact() {
+  document.getElementById('overlay-fact').style.display = 'none';
 }
