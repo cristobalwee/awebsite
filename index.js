04091e43d2cc4436019e07d48f35973f7f997aaf
Cristobal Grana <cristobal.wee@gmail.com>
2018-06-19 13:35:49 -0500

    Fixed styles

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+var facts = [
+  "Typeface vs. Font",
+  "Ale vs. Lager",
+  "Whiskey vs. Bourbon",
+  "Fax machines",
+  "Edison"
+];
+
 function openAbout() {
   document.getElementById('overlay-about').style.display = 'block';
 }
@@ -7,7 +15,25 @@ function closeAbout() {
 }
 
 function openFact(fact) {
-  document.getElementById('fact-content').innerHTML = '<h2>' + fact + '</h2>' + '<p>facts</p>';
+  var idx = 0;
+  switch(fact) {
+    case 'beer':
+      idx = 1;
+      break;
+    case 'whiskey':
+      idx = 2;
+      break;
+    case 'fax':
+      idx = 3;
+      break;
+    case 'edison':
+      idx = 4;
+      break;
+    default:
+      break;
+  }
+
+  document.getElementById('fact-content').innerHTML = '<h2>' + facts[idx] + '</h2>' + '<p>' + fact + '</p>';
   document.getElementById('overlay-fact').style.display = 'block';
 }
 
